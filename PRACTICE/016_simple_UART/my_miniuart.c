ae040064b31a40bc37dd608e3b1bac16a6bc87b1
taktopanie <maciejzwolan@gmail.com>
2025-11-19 16:21:55 +0100

    Kernel Timer add for STM connection indication

--- PRACTICE/016_simple_UART/my_miniuart.c
+++ PRACTICE/016_simple_UART/my_miniuart.c
@@ -11,6 +11,12 @@
 static void __iomem *uart_base;
 static int major;
 
+//Timers
+static struct timer_list TIMER_1;
+
+char LED_ON_CMD[] = "#1q\n";
+char LED_OFF_CMD[] = "#0q\n";
+
 // Mini UART register offsets
 #define AUX_MU_IO_REG     0x00
 #define AUX_MU_LSR_REG    0x14
@@ -89,8 +95,6 @@ long my_dev_ioctl (struct file *filp, unsigned int cmd, unsigned long arg)
 #ifdef DEBUG_PRINT
     pr_info("IOCTL CALLED\n");
 #endif
-    char LED_ON_CMD[] = "#1q\n";
-    char LED_OFF_CMD[] = "#0q\n";
 
     int i = 0;
     switch(cmd) 
@@ -130,6 +134,37 @@ long my_dev_ioctl (struct file *filp, unsigned int cmd, unsigned long arg)
 
 }
 
+/*
+    THIS timer is called each 1s to inform that module is cooperating with STM
+*/
+uint8_t LED_STATE = 0;
+
+void my_timer_callback(struct timer_list * t)
+{
+#ifdef DEBUG_PRINT
+    pr_info("rpi_mini_uart: TIMER_1 expired\n");
+#endif
+    //LED toggle
+    if(LED_STATE)
+    {
+        for(int i = 0; i < strlen(LED_OFF_CMD); i++)
+        {
+            UART_hw_write_char(LED_OFF_CMD[i]);
+        }
+        LED_STATE = 0;
+    }else
+    {
+        for(int i = 0; i < strlen(LED_ON_CMD); i++)
+        {
+            UART_hw_write_char(LED_ON_CMD[i]);
+        }
+        LED_STATE = 1;
+    }
+
+    //set timer to 1s again
+    mod_timer(&TIMER_1, jiffies + msecs_to_jiffies(1000));
+}
+
 static struct file_operations rpi_uart_fops = {
     .owner = THIS_MODULE,
     .open = rpi_uart_open,
@@ -143,18 +178,26 @@ static int __init rpi_uart_init(void)
 {
     pr_info("rpi_mini_uart: initializing\n");
 
+    //map UART registry to __iomem *uart_base;
     uart_base = ioremap(MINI_UART_BASE, MINI_UART_SIZE);
     if (!uart_base) {
         pr_err("rpi_mini_uart: unable to map UART registers\n");
         return -ENOMEM;
     }
 
+    //make char device
     major = register_chrdev(0, DRIVER_NAME, &rpi_uart_fops);
     if (major < 0) {
         iounmap(uart_base);
         pr_err("rpi_mini_uart: failed to register chrdev\n");
         return major;
     }
+
+    //setup timer TIMER_1 for UART led toggling on STM
+    timer_setup(&TIMER_1, my_timer_callback, 0);
+
+    //jiffies(actual time) + 1000 ms = 1s [callback timer]
+    mod_timer(&TIMER_1, jiffies + msecs_to_jiffies(1000));
 
     pr_info("rpi_mini_uart: registered with major %d\n", major);
     return 0;
@@ -165,6 +208,9 @@ static void __exit rpi_uart_exit(void)
     unregister_chrdev(major, DRIVER_NAME);
     iounmap(uart_base);
     pr_info("rpi_mini_uart: exited\n");
+
+    //delete timer
+    del_timer_sync(&TIMER_1);
 }
 
 module_init(rpi_uart_init);
